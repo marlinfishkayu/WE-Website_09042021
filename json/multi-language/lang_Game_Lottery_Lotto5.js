8477f217e1afee2ff18a72647c991e0f6402f298
kate <youremail@aol.com>
2021-04-09 16:51:59 +0800

    Update the hero banner

---
+++ json/multi-language/lang_Game_Lottery_Lotto5.js
@@ -0,0 +1,324 @@
+var arrLang = {
+    'EN': {
+      //navbar
+      'current-lang': 'En',
+      'nav_About': 'About WE',
+      'nav_LiveGames': 'Games',
+      'nav_LiveStudio': 'Live Studio',
+      'nav_Services': 'Services',
+      'nav_WhatsNew': "What's New",
+      'nav_ContactUs': 'Contact Us',
+      'nav_Search': 'Search our website',
+      
+      //navbar-submenu
+      'submenu_LiveGames': 'Live Games',
+      'submenu_Lottery': 'Lottery',
+      'submenu_Sportsbook': 'Sportsbook',
+      'submenu_Slots': 'Slots',
+      'submenu_AllLiveGame': 'All Live Games',
+      'submenu_AllLotteryGame': 'All Lottery Games',
+      'ComingSoon': 'COMING SOON',
+  
+      //livegame submenu
+      'Blockchain': 'Blockchain',
+      'BlockchainBaccarat': 'Blockchain Baccarat',
+      'BlockchainDragonTiger': 'Blockchain Dragon Tiger',
+      'BlockchainSpeedBaccarat': 'Blockchain Speed Baccarat',
+      'BlockchainSqueezeBaccarat': 'Blockchain Squeeze Baccarat',
+      'Baccarat': 'Baccarat',
+      'Roulette': 'Roulette',
+      'SicBo': 'Sic Bo',
+      'DragonTiger': 'Dragon Tiger',
+      'MahjongLuckyWheel': 'Mahjong Lucky Wheel',
+      'SqueezeBaccarat': 'Squeeze Baccarat',
+      'SpeedBaccarat': 'Speed Baccarat',
+      'GodofFortuneSicBo': 'God of Fortune Sic Bo',
+      'GodofFortuneRoulette': 'God of Fortune Roulette',
+      'WinThreeCards': 'Win Three Cards',
+      'Mahjong': 'Mahjong',
+      'LuckyWheel': 'Lucky Wheel',
+      'GodofFortune': 'God of Fortune',
+      'Lotto5': 'Lotto-5',
+      'PK10': 'PK-10 Racing',
+      //Game Slogan
+      'Baccarat_slogan': 'WE play beyond classic.',
+      'Roulette_slogan': 'Big win on European classic.',
+      'SicBo_slogan': 'An unparalleled game of chance.',
+      'DragonTiger_slogan': 'The perfect game for quick players.',
+      'MahjongLuckyWheel_slogan': 'Double fun! Triple excitement! ',
+      'SqueezeBaccarat_slogan': 'Prolong the excitement and tension.',
+      'GodofFortuneSicBo_slogan': 'Prizing you unexceptionally.',
+      'GodofFortuneRoulette_slogan': 'Spin beyond the classic.',
+      'WinThreeCards_slogan': 'A game to challenge your intelligence.',
+      'SpeedBaccarat_slogan': 'Speedy and mind-blowing.',
+      'Lotto5_slogan': 'Thrill ball game with attractive odds.',
+      'PK10_slogan': 'Let’s PK the racing',
+  
+      //Cookie popup
+      'Cookie_p1' : 'WE use cookies to ensure you get the best experience on our website. WE will assume you have agreed to our Privacy Policy by continuing to use our website henceforth.',
+      'Accept' : 'Accept',
+      //Footer
+      'WorldEnt' : '©2020 WORLD ENTERTAINMENT.',
+      'Subscribe' : 'Subscribe',
+      'SignUp' : 'Sign Up',
+      'Subscribe_p' : 'Be the first to know about the latest news and exclusive offers from WE.',
+      'LicCert' : 'Licences & Certifications',
+      'PrivacyPolicy' : 'Privacy Policy',
+      'HelloThere' : 'Hello There!',
+      'thankyou_p' : 'You’re on our mailing list now and will be among the first to hear about the latest news and exclusive offers from WE.',
+      'Okay' : 'OKAY',
+      //input placeholder
+      'input_email':'Please enter your email address here',
+      
+      //Game Content
+      'try_now': 'try now',
+      'AllLotteryGame': 'All Lottery Games',
+      'feature': 'FEATURE HIGHLIGHTS',
+      'compatible': 'compatible with',
+      'leading': 'Leading the way, play new.',
+      'try_our_games': 'Try Our Games',
+      //
+      'Lottery':'LOTTERY',
+      'gameDetail_p1': 'Lotto-5 is a popular lottery across China. It is an interesting game and includes a variety of betting choices with attractive odds. Players need to bet on the combination of numbers in order to win the game.',
+      'feature_1.1': 'Massive betting',
+      'feature_1.2': 'options',
+      'feature_1_p': 'Lotto-5 offers a wide range of betting choices with different odds for selection. Players can take the chances of winning bigger and enchanting awards.',
+      'feature_2.1': 'Customize chip',
+      'feature_2.2': 'amount',
+      'feature_2_p': 'WE offer the standardized chips, but players are also able to set the chip amounts in “Entertainment” mode to win more generous bonuses.',
+      'feature_3.1': 'Grasp the game',
+      'feature_3.2': 'tendency',
+      'feature_3_p': 'WE provide a series of statistics which include comprehensive scoreboards and results. It saves the players’ preparation time before betting through displaying the trend of the game in the chart clearly.',
+    },
+  
+  
+    // 繁體中文
+    'TC': {
+      //navbar
+      'current-lang': '繁',
+      'nav_About': '關於WE',
+      'nav_LiveGames': '遊戲',
+      'nav_LiveStudio': '攝影棚',
+      'nav_Services': '服務',
+      'nav_WhatsNew': '最新消息',
+      'nav_ContactUs': '聯絡我們',
+      'nav_Search': '搜索我們的網站',
+  
+      //navbar-submenu
+      'submenu_LiveGames': '真人遊戲',
+      'submenu_Lottery': '彩票',
+      'submenu_Sportsbook': '體育博彩',
+      'submenu_Slots': '老虎機',
+      'submenu_AllLiveGame': '顯示全部',
+      'submenu_AllLotteryGame': '顯示全部',
+      'ComingSoon': '快將推出',
+  
+      //livegame submenu
+      'Blockchain': '區塊鏈',
+      'BlockchainBaccarat': '區塊鏈百家樂',
+      'BlockchainDragonTiger': '區塊鏈龍虎',
+      'BlockchainSpeedBaccarat': '區塊鏈極速百家樂',
+      'BlockchainSqueezeBaccarat': '區塊鏈咪牌百家樂',
+      'Baccarat': '百家樂',
+      'Baccarat_slogan': '保留經典，再創高峰！',
+      'Roulette': '輪盤',
+      'SicBo': '骰寶',
+      'DragonTiger': '龍虎',
+      'MahjongLuckyWheel': '麻將幸運輪',
+      'SqueezeBaccarat': '共咪百家樂',
+      'SpeedBaccarat': '極速百家樂',
+      'GodofFortuneSicBo': '財神骰寶',
+      'GodofFortuneRoulette': '財神輪盤',
+      'WinThreeCards': '炸金花',
+      'Mahjong': '麻將',
+      'LuckyWheel': '幸運輪',
+      'GodofFortune': '財神',
+      'Lotto5': '時時彩',
+      'PK10': '北京賽車',
+      //Game Slogan
+      'Baccarat_slogan': '保留經典，再創高峰！',
+      'Roulette_slogan': '歐洲輪盤之癲',
+      'SicBo_slogan': '無可比擬的機會遊戲',
+      'DragonTiger_slogan': '速戰速決',
+      'MahjongLuckyWheel_slogan': '雙重享受．三倍刺激',
+      'SqueezeBaccarat_slogan': '延長興奮和刺激感',
+      'GodofFortuneSicBo_slogan': '意想不到的派彩',
+      'GodofFortuneRoulette_slogan': '挑戰經典的全新力作',
+      'WinThreeCards_slogan': '一個「炸」腦的遊戲',
+      'SpeedBaccarat_slogan': '高速快感 讓人心跳加速',
+      'Lotto5_slogan': '最高賠率彩球遊戲',
+      'PK10_slogan':'競速賽車，等你來 PK',
+  
+      //Cookie popup
+      'Cookie_p1' : '本網站使用COOKIES以確保你在我們網站上獲得最佳的使用體驗。如果你繼續使用本網站，這將被認為你已同意本網站的隱私政策 。',
+      'Accept' : '同意',
+      //Footer
+      'WorldEnt' : '©2020 世界娛樂',
+      'Subscribe' : '訂閱',
+      'SignUp' : '立即註冊',
+      'Subscribe_p' : '成為第一個收到WE最新消息和獨家優惠的人。',
+      'LicCert' : '營業執照及國際認證',
+      'PrivacyPolicy' : '隱私政策',
+      'HelloThere' : 'Hello～！',
+      'thankyou_p' : '你現在已在我們的聯絡名單中，我們會將 WE 的最新消息和獨家優惠通過電郵發送給你。',
+      'Okay' : '關閉',
+      //input placeholder
+      'input_email':'請在此處輸入你的電子郵件地址',
+    
+      //Game Content
+      'try_now': '立即試玩',
+      'AllLotteryGame': '彩票',
+      'feature': '功能亮點',
+      'compatible': '支持平台',
+      'leading': '一路領先，搶鮮玩！',
+      'try_our_games': '免費試玩',
+      //
+      'Lottery':'彩票',
+      'gameDetail_p1': '時時彩是紅遍全中國的彩票遊戲。遊戲不僅富有趣味性，投注方式也是五花八門，同時具有吸引力的高賠率獎金。玩家只需要押中開獎彩球號碼的排列組合，豐厚獎金隨即奉上。',
+      'feature_1.1': '投注選項',
+      'feature_1.2': '豐富多變',
+      'feature_1_p': '「時時彩」備有多種投注組合可供玩家選擇，且每款組合的派彩回報也各有不同，賠率千變萬化，玩家隨時搏取更誘人的獎金。',
+      'feature_2.1': '自定籌碼金額',
+      'feature_2.2': '',
+      'feature_2_p': '除了系統預設的籌碼外，玩家可跳轉至「娛樂」模式，設定不一樣的籌碼金額，贏取更豐盛的獎金。',
+      'feature_3.1': '數據 掌握',
+      'feature_3.2': '遊戲走勢',
+      'feature_3_p': '我們提供一系列的數據提示，包括全方位路紙和開獎結果；透過數據走勢分析開獎結果，以圖表方式顯示，一目了然，為玩家節省下注前的準備時間。',
+    },
+
+
+    // 简体中文
+    'SC': {
+      //navbar
+      'current-lang': '简',
+      'nav_About': '关於WE',
+      'nav_LiveGames': '游戏',
+      'nav_LiveStudio': '摄影棚',
+      'nav_Services': '服务',
+      'nav_WhatsNew': '最新消息',
+      'nav_ContactUs': '联络我们',
+      'nav_Search': '搜索我们的网站',
+  
+      //navbar-submenu
+      'submenu_LiveGames': '真人游戏',
+      'submenu_Lottery': '彩票',
+      'submenu_Sportsbook': '体育博彩',
+      'submenu_Slots': '老虎机',
+      'submenu_AllLiveGame': '显示全部',
+      'submenu_AllLotteryGame': '显示全部',
+      'ComingSoon': '快将推出',
+  
+      //livegame submenu
+      'Blockchain': '区块链',
+      'BlockchainBaccarat': '区块链百家乐',
+      'BlockchainDragonTiger': '区块链龙虎',
+      'BlockchainSpeedBaccarat': '区块链极速百家乐',
+      'BlockchainSqueezeBaccarat': '区块链咪牌百家乐',
+      'Baccarat': '百家乐',
+      'Baccarat_slogan': '保留经典，再创高峰！',
+      'Roulette': '轮盘',
+      'SicBo': '骰宝',
+      'DragonTiger': '龙虎',
+      'MahjongLuckyWheel': '麻将幸运轮',
+      'SqueezeBaccarat': '共咪百家乐',
+      'SpeedBaccarat': '极速百家乐',
+      'GodofFortuneSicBo': '财神骰宝',
+      'GodofFortuneRoulette': '财神轮盘',
+      'WinThreeCards': '炸金花',
+      'Mahjong': '麻将',
+      'LuckyWheel': '幸运轮',
+      'GodofFortune': '财神',
+      'Lotto5': '时时彩',
+      'PK10': '北京赛车',
+      //Game Slogan
+      'Baccarat_slogan': '保留经典，再创高峰！',
+      'Roulette_slogan': '欧洲轮盘之癫',
+      'SicBo_slogan': '无可比拟的机会游戏',
+      'DragonTiger_slogan': '速战速决',
+      'MahjongLuckyWheel_slogan': '双重享受．三倍刺激',
+      'SqueezeBaccarat_slogan': '延长兴奋和刺激感',
+      'GodofFortuneSicBo_slogan': '意想不到的派彩',
+      'GodofFortuneRoulette_slogan': '挑战经典的全新力作',
+      'WinThreeCards_slogan': '一个「炸」脑的游戏',
+      'SpeedBaccarat_slogan': '高速快感 让人心跳加速',
+      'Lotto5_slogan': '最高赔率彩球游戏',
+      'PK10_slogan':'竞速赛车，等你来 PK',
+  
+      //Cookie popup
+      'Cookie_p1' : '本网站使用COOKIES以确保你在我们网站上获得最佳的使用体验。如果你继续使用本网站，这将被认为你已同意本网站的隐私政策 。',
+      'Accept' : '同意',
+      //Footer
+      'WorldEnt' : '©2020 世界娱乐',
+      'Subscribe' : '订阅',
+      'SignUp' : '立即注册',
+      'Subscribe_p' : '成为第一个收到WE最新消息和独家优惠的人。',
+      'LicCert' : '营业执照及国际认证',
+      'PrivacyPolicy' : '隐私政策',
+      'HelloThere' : 'Hello～！',
+      'thankyou_p' : '你现在已在我们的联络名单中，我们会将 WE 的最新消息和独家优惠通过电邮发送给你。',
+      'Okay' : '关闭',
+      //input placeholder
+      'input_email':'请在此处输入你的电子邮件地址',
+    
+      //Game Content
+      'try_now': '立即试玩',
+      'AllLotteryGame':'彩票',
+      'feature': '功能亮点',
+      'compatible': '支持平台',
+      'leading': '一路领先，抢鲜玩！',
+      'try_our_games': '免费试玩',
+      //
+      'Lottery':'彩票',
+      'gameDetail_p1': '时时彩是红遍全中国的彩票游戏。游戏不仅富有趣味性，投注方式也是五花八门，同时具有吸引力的高赔率奖金。玩家只需要押中开奖彩球号码的排列组合，丰厚奖金随即奉上。',
+      'feature_1.1': '投注选项',
+      'feature_1.2': '丰富多变',
+      'feature_1_p': '「时时彩」备有多种投注组合可供玩家选择，且每款组合的派彩回报也各有不同，赔率千变万化，玩家随时搏取更诱人的奖金。',
+      'feature_2.1': '自定筹码金额',
+      'feature_2.2': '',
+      'feature_2_p': '除了系统预设的筹码外，玩家可跳转至「娱乐」模式，设定不一样的筹码金额，赢取更丰盛的奖金。',
+      'feature_3.1': '数据 掌握',
+      'feature_3.2': '遊戲走勢',
+      'feature_3_p': '我们提供一系列的数据提示，包括全方位路单和开奖结果；透过数据走向分析开奖结果，以图表方式显示，一目了然，为玩家节省下注前的准备时间。',
+    }
+  };
+  
+
+//change image language
+var arrImg = {
+  'EN': [{
+      //WE Logo
+      'WE_logo': 'images/icon/en/logo.svg',
+      //Lotto5 Logo
+      'Lotto5_icon': 'images/icon/en/game_icon_Lotto5.svg',
+      //UI
+      'UI': 'images/game/game_Lotto5/en/game_UI_Lotto5_x1.png 500w, images/game/game_Lotto5/en/game_UI_Lotto5_x2.png 1000w',
+      //Feature
+      'feature1': 'images/game/game_Lotto5/en/game_Lotto5_feature1_1x.png 500w, images/game/game_Lotto5/en/game_Lotto5_feature1_2x.png 1000w',
+      'feature2': 'images/game/game_Lotto5/en/game_Lotto5_feature2_1x.png 500w, images/game/game_Lotto5/en/game_Lotto5_feature2_2x.png 1000w',
+      'feature3': 'images/game/game_Lotto5/en/game_Lotto5_feature3_1x.png 500w, images/game/game_Lotto5/en/game_Lotto5_feature3_2x.png 1000w',
+    }],
+  'TC': [{
+      //WE Logo
+      'WE_logo': 'images/icon/tc/logo.svg',
+      //Lotto5 Logo
+      'Lotto5_icon': 'images/icon/tc/game_icon_Lotto5.svg',
+      //UI
+      'UI': 'images/game/game_Lotto5/tc/game_UI_Lotto5_x1.png 500w, images/game/game_Lotto5/tc/game_UI_Lotto5_x2.png 1000w',
+      //Feature
+      'feature1': 'images/game/game_Lotto5/tc/game_Lotto5_feature1_1x.png 500w, images/game/game_Lotto5/tc/game_Lotto5_feature1_2x.png 1000w',
+      'feature2': 'images/game/game_Lotto5/tc/game_Lotto5_feature2_1x.png 500w, images/game/game_Lotto5/tc/game_Lotto5_feature2_2x.png 1000w',
+      'feature3': 'images/game/game_Lotto5/tc/game_Lotto5_feature3_1x.png 500w, images/game/game_Lotto5/tc/game_Lotto5_feature3_2x.png 1000w',  
+    }],
+  'SC': [{
+      //WE Logo
+      'WE_logo': 'images/icon/sc/logo.svg',
+      //Lotto5 Logo
+      'Lotto5_icon': 'images/icon/sc/game_icon_Lotto5.svg',
+      //UI
+      'UI': 'images/game/game_Lotto5/sc/game_UI_Lotto5_x1.png 500w, images/game/game_Lotto5/sc/game_UI_Lotto5_x2.png 1000w',
+      //Feature
+      'feature1': 'images/game/game_Lotto5/sc/game_Lotto5_feature1_1x.png 500w, images/game/game_Lotto5/sc/game_Lotto5_feature1_2x.png 1000w',
+      'feature2': 'images/game/game_Lotto5/sc/game_Lotto5_feature2_1x.png 500w, images/game/game_Lotto5/sc/game_Lotto5_feature2_2x.png 1000w',
+      'feature3': 'images/game/game_Lotto5/sc/game_Lotto5_feature3_1x.png 500w, images/game/game_Lotto5/sc/game_Lotto5_feature3_2x.png 1000w',  
+    }]
+};
